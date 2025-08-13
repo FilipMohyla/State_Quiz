505d0754c739496199e56c26ef8784ed47b161ae
Filip Mohyla <mohyla.filip@seznam.cz>
2025-08-13 19:43:18 +0200

    First setup of settings, tables for states and capitals created

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    
+    state_name = models.CharField()
+
+class Capital(models.Model):
+
+    capital_name = models.OneToOneField(State, on_delete=models.CASCADE)
