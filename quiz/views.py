183eebad6c9a45815e1dc584ce3f0b2417cdb1c1
Filip Mohyla <mohyla.filip@seznam.cz>
2025-08-19 20:42:14 +0200

    first app  design

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,34 @@
+# quiz/views.py
+import random
 from django.shortcuts import render
+from .models import Country
 
-# Create your views here.
+def quiz_view(request):
+    if request.method == 'POST':
+        country_id = int(request.POST['country_id'])
+        user_answer = request.POST['answer']
+        
+        country = Country.objects.get(id=country_id)
+        is_correct = (user_answer == country.capital)
+
+        return render(request, 'result.html', {
+            'country': country,
+            'user_answer': user_answer,
+            'is_correct': is_correct
+        })
+
+    # GET request – náhodná otázka
+    country = random.choice(list(Country.objects.all()))
+    
+    # vybereme 3 náhodné jiné možnosti
+    wrong_options = list(
+        Country.objects.exclude(id=country.id).order_by('?')[:3]
+    )
+    
+    options = [country.capital] + [c.capital for c in wrong_options]
+    random.shuffle(options)  # zamícháme
+    
+    return render(request, 'questions.html', {
+        'country': country,
+        'options': options
+    })
